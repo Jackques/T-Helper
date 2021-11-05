73d40bb8ab18988b3aca9fda1c4c597322aac960
Jack <Gommeren@msn.com>
2021-11-05 23:03:26 +0100

    Added console logs to track clicking the Activate button

--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -1,9 +1,12 @@
 import $ from "jquery";
 
 document.addEventListener('DOMContentLoaded', function () {
+    console.log('Clicked activate');
     $('#jack').on('click', function () {
+        console.log('Processing click');
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs:chrome.tabs.Tab[]) {
             if(tabs.length > 0 && tabs[0].id){
+                console.log('Sending message to content');
                 chrome.tabs.sendMessage(tabs[0].id, {type:"Activate"}, function(response){
                     console.log('Do i need a callback?');
                 });
