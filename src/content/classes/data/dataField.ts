b4e9e57f7945eff8e80350daa9c1900db1067c0e
Jack <Gommeren@msn.com>
2021-11-15 00:47:19 +0100

    Updated dataField & renamed dataStore to dataRecord
    DataField now uses some type checking logic

--- src/content/classes/data/dataField.ts
+++ src/content/classes/data/dataField.ts
@@ -1,29 +1,45 @@
-class DataField {
+import { logicContainer } from "src/content/interfaces/logicContainer.interface";
+
+export class DataField {
     public title: string;
     public description: string;
+    public requiredField: boolean;
+    public requiresUI: boolean;
     public multipleDataEntry: boolean;
     public mustBeUnique: boolean;
     public autoGather: boolean;
     public onlyGatherOnce: boolean;
+
+    private dataLogic: logicContainer;
     public dataEntries: any[] = [];
 
-    constructor(title:string, description:string, multipleDataEntry:boolean, mustBeUnique:boolean, autoGather:boolean, onlyGatherOnce:boolean){
+    constructor(title:string, description:string, requiredField:boolean, requiresUI:boolean, multipleDataEntry:boolean, mustBeUnique:boolean, autoGather:boolean, onlyGatherOnce:boolean, dataLogic: logicContainer){
         this.title = title;
         this.description = description;
+        this.requiredField = requiredField;
+        this.requiresUI = requiresUI; //determines if the fields is visible in UI
         this.multipleDataEntry = multipleDataEntry;
         this.mustBeUnique = mustBeUnique;
         this.autoGather = autoGather;
         this.onlyGatherOnce = onlyGatherOnce;
+
+        this.dataLogic = dataLogic;
+
+        //todo: e.g. autoGather; if true, then check in the provided dataSource if e.g. a numnber already exists. if not assign a new (increment from the former) number to this person
     }
 
-    public addDataEntry(dataEntry:any):void {
-        // todo: Create dataEntry class & objects for here to add. Properties must be: date & time? the data itself (depends on each type of data), auto-checking correct type of data received, etc? 
+    public addDataEntry(dataEntry:unknown):void {
+        // todo: Create dataEntry class & objects for here to add. Properties must be: an incremented no, date & time? the data itself (depends on each type of data), auto-checking correct type of data received, etc? 
 
         // here in this method i do: check if the added data..
         // 1. can be added due to multipleDataEntry setting?
         // 2. must be unique check? if checked, check if given data is unique
         // 3. autoGather.. so idk? call something which gathers the data for this classobject?
         // 4. if one entry already exists, prevent the addition of multiple entries (e.g. i can change my attractiveness rating of a girl afterwards thus allowing multiple, but if i got a match with her or not is really only added once, thus should be prevented from adding the same data again)
+        if(!this.isDataEntryValid(dataEntry)){
+            console.error('The data is not valid');
+        }
+        //todo: actually enter the data
     }
 
     public getTypeOfRequiredData(){
@@ -32,6 +48,44 @@ class DataField {
         1. check if headers equal the headers defined here
         2. check if the data structure (e.g. date format) equals the data structure used in the data when reading the data (and provide a clear error indicating line and data when not and what the data structure SHOULD be)
         */
+    }
+    public isDataEntryValid(dataEntry: unknown): boolean {
+        if(this.dataLogic.baseType === 'string'){
+            if(this.dataLogic.checkMethod !== null){
+                if(this.dataLogic.checkMethod(dataEntry)){
+                    return true;
+                }
+                return typeof dataEntry === 'string' || dataEntry instanceof String;
+            }
+        }
+
+        if(this.dataLogic.baseType === 'boolean'){
+            if(this.dataLogic.checkMethod !== null){
+                if(this.dataLogic.checkMethod(dataEntry)){
+                    return true;
+                }
+                return typeof dataEntry === 'boolean' || dataEntry instanceof Boolean;
+            }
+        }
+
+        if(this.dataLogic.baseType === 'number'){
+            if(this.dataLogic.checkMethod !== null){
+                if(this.dataLogic.checkMethod(dataEntry)){
+                    return true;
+                }
+                return typeof dataEntry === 'number' || dataEntry instanceof Number;
+            }
+        }
+        
+        if(this.dataLogic.baseType === 'list'){
+            if(this.dataLogic.checkMethod === null){
+                console.error(`${this.title} lacks a checkMethod whilst baseType is a list`);
+                return false;
+            }
+            //todo: continue here!
+            console.error('lists not yet defined');
+        }
+        return false;
     }
 
 }
---
+++ src/content/classes/data/dataRecord.ts
@@ -0,0 +1,71 @@
+import { DataField } from "./dataField";
+import { dataDate } from "./dataLogic/dataDate";
+
+class DataRecord {
+        /*
+        Output:
+
+        No                          - int
+        Datum-liket                 - string datetime
+        Naam                        - string any
+        Leeftijd                    - int
+        Heeft-profieltekst          - boolean
+        Heeft-zinnige-profieltekst  - boolean
+        Geverifieerd                - boolean
+        Aantrekkelijkheidsscore     - int
+        Match                       - boolean
+        Datum-match                 - string datetime 
+        Ander-eerste-bericht        - boolean
+        Ander-gereageerd            - boolean
+        Gesprek-op-gang             - boolean
+        Gevoel-van-gemak-gesprek    - int
+        Hoe-vaak-ghost              - [ {'datetime after time expired', 'time passed since my last message'} ]
+        Nummer-verkregen            - boolean
+        Reactie-snelheid            - [ {'time passed between my message to person and their first response'} ]
+        Blocked-of-geen-contact     - string 'blocked' |  'said-no-contact' | 'available'
+        Geinteresseerd-sex          - boolean
+        Potentiele-klik             - boolean
+        Notities                    - string any
+        */
+
+    
+    public usedDataFields:DataField[] = [
+        new DataField('No', 'The number of the person', true, false, false, true, true, true, {baseType: 'number', checkMethod: null}),
+            new DataField('Datum-liket', 'The datetime when I gave the like/sent my first message', true, false, false, false, true, true, {baseType: 'string', checkMethod: dataDate.isDate}),
+        new DataField('Naam', 'The name of the person', true, false, false, false, true, true, {baseType: 'string', checkMethod: null}),
+        new DataField('Leeftijd', 'The age of the person', false, false, false, false, true, true, {baseType: 'number', checkMethod: null}), // preferably getting this by birthdate otherwise just the age number is fine too
+        new DataField('Heeft-profieltekst', 'Wether or not this person has some text on the profile', true, false, false, false, true, true, {baseType: 'boolean', checkMethod: null}),
+        new DataField('Heeft-zinnige-profieltekst', 'Wether or not this person has some text on the profile', true, true, false, false, false, false, {baseType: 'boolean', checkMethod: null}),
+        new DataField('Geverifieerd', 'Wether or not this person has some text on the profile', true, false, false, false, true, true, {baseType: 'boolean', checkMethod: null}),
+            new DataField('Aantrekkelijkheidsscore', '', true, true, true, false, false, false, {baseType: 'number', checkMethod: null}), // NOTE! attractiveness rating on photo's can be 1, 2, 3, 6.5, 6, 7,5, 8 etc. but also: NAN (no photo available when there is litterally no photo?)
+        new DataField('Match', 'Wether we have a match/she responded or not', true, false, false, false, true, false, {baseType: 'boolean', checkMethod: null}),
+            new DataField('Datum-match', 'The datetime when I and the person had a match', true, false, false, false, true, false, {baseType: 'string', checkMethod: dataDate.isDate}),
+        new DataField('Ander-eerste-bericht', 'If this person sent me a first message', true, false, false, false, true, false, {baseType: 'boolean', checkMethod: null}), // not-yet (if she did not yet send me a message within a certain time-period), no (if she did not send me a first message within a certain timeperiod after being matched), yes (if she did send me a first message within a certain time-period)
+        new DataField('Ander-gereageerd', 'If this person responded to my first message', true, false, false, false, true, false, {baseType: 'boolean', checkMethod: null}),
+        new DataField('Gesprek-op-gang', 'If this person sent at least one message in between my first 3 messages', true, false, false, false, true, false, {baseType: 'boolean', checkMethod: null}),
+            new DataField('Gevoel-van-gemak-gesprek', 'The feeling of how easy & fun it is to have a conversation with this person ranging from 1 (very responsive & fun) to 6 (hardly responsive & teeth pulling)', false, true, true, false, false, false, {baseType: 'integer', checkMethod: null}),
+            new DataField('Hoe-vaak-ghost', 'How many times this person did not respond in a certain timeframe', false, false, true, false, true, false, {baseType: 'list', checkMethod: null}),
+        new DataField('Nummer-verkregen', 'Did I get further contact details (e.g. phone number) from this person?', false, true, false, false, false, false, {baseType: 'boolean', checkMethod: null}),
+            new DataField('Reactie-snelheid', 'The moments of time between each response', false, false, true, false, true, false, {baseType: 'list', checkMethod: null}), // de dagen & tijden tussen de eerste nieuwe berichten vanuit de ander
+        new DataField('Blocked-of-geen-contact', 'If this person blocked me/deleted our conversation or indicated they did not wish further contact', false, true, false, false, true, false, {baseType: 'boolean', checkMethod: null}), // needs a UI too!
+        new DataField('Geinteresseerd-sex', 'Wether this person has indicated to be interested in a hookup or not', false, true, false, false, false, false, {baseType: 'boolean', checkMethod: null}),
+        new DataField('Potentiele-klik', 'Wether the vibe of the conversation was good enough to say "we clicked"', false, true, false, false, false, false, {baseType: 'boolean', checkMethod: null}),
+        new DataField('Notities', 'Any interesting notes on this person', false, true, false, false, false, false, {baseType: 'string', checkMethod: null}),
+    ];
+
+    constructor(){
+
+    }
+
+    /*  ZET HIER WELKE TAGS IK MOMENTEEL WEL GA ONDERSTEUNEN EN WELKE NIET! BEGIN KLEIN!
+    'IsFake', // because maybe I want to keep track of how many fake profiles I encounter on any given app
+    'howManyReminders' // because I want to keep track of how many reminders I sent.. AND if they worked?
+    'Woonplaats', // because maybe I want to keep track of the distance between me and my (potential) match so I can take that variabele into account if many matches ghost e.g. because they may be too far away (tinder/happn happens to show me the distance, if not availble I can get the city at least (or ask the person myself) thus use this to calculate the distance from me: https://www.distance24.org/api.xhtml)
+    'gaf-mij-compliment', // because maybe I want to keep track of how many compliments (physical? or about the personality?) this profile gets..
+    'vibe-tags', // because I want to keep track of the characteristics // vibe i get from this person; religious, trashy, into-sports, outdoorsy, nerdy, dominant, submissive, sexual, etc.
+        ZET HIER WELKE TAGS IK MOMENTEEL WEL GA ONDERSTEUNEN EN WELKE NIET! BEGIN KLEIN! */
+
+    // public getUsedDataFieldByName(title: string){
+    //     //todo: method which !!POPUP!! can use to get the correct DataField object from this internal list
+    // }
+}
