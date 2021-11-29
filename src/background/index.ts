6b1d536d8931ab36ae0f1ed2e94b3395b972c010
Jack <Gommeren@msn.com>
2021-11-29 16:52:31 +0100

    Added todo for feature-wishlist

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,5 +1,12 @@
 // import { requestInterceptor } from "./requestInterceptor";
 // console.log('Hey! This code is executed in the background, you will not see it in the browser console...');
 
+import { workers } from "cluster"
+
 // const test = new requestInterceptor();
 // console.dir(`jackbg xAuthToken: ${test.xAuthToken}`);
+
+// TODO:
+// maybe here i could (use the list from manifest.json) or have a seperate list here of usable websites/apps..
+// on which this app workers.. and if it works on said Website
+// enable the browserextension for activation?
