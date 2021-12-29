980a84a5b7a6166a2754f353712597f2e819d213
Jack <Gommeren@msn.com>
2021-12-29 21:33:33 +0100

    Expanded dateHelper class with isDateBetweenGreaterThanAmountOfDays, getAmountDaysBetweenDates and getAmountMilisecondesBetweenDates methods

--- src/content/classes/util/dateHelper.ts
+++ src/content/classes/util/dateHelper.ts
@@ -10,6 +10,31 @@ export class DateHelper {
     }
 
     static isValidDate(dateString: string): boolean {
+        // hint: e.g. "2021-04-28T20:01:16.126Z" (ISO stringdates are valid dates)
         return moment(dateString).isValid();
+    }
+
+    static isDateBetweenGreaterThanAmountOfDays(dateOne: string, dateTwo: string, amountOfDays: number): boolean | undefined {
+        if(amountOfDays <= 0){
+            console.error(`The amount of days cannot be 0 or less`);
+            return undefined;
+        }
+        return this.getAmountDaysBetweenDates(dateOne, dateTwo) !== undefined && <number>this.getAmountDaysBetweenDates(dateOne, dateTwo) > amountOfDays ? true : false;
+    }
+
+    static getAmountDaysBetweenDates(dateFormer: string, dateLater: string): number | undefined {
+        if(!DateHelper.isValidDate(dateFormer) || !DateHelper.isValidDate(dateLater)){
+            console.error(`Parameter dateOne: ${dateFormer} and/or dateTwo: ${dateLater} is not a valid date`);
+            return undefined;
+        }
+        return moment.duration(moment(dateLater).diff(dateFormer)).asDays();
+    }
+
+    static getAmountMilisecondesBetweenDates(dateFormer: string, dateLater: string): number | undefined {
+        if(!DateHelper.isValidDate(dateFormer) || !DateHelper.isValidDate(dateLater)){
+            console.error(`Parameter dateOne: ${dateFormer} and/or dateTwo: ${dateLater} is not a valid date`);
+            return undefined;
+        }
+        return moment.duration(moment(dateLater).diff(dateFormer)).asMilliseconds();
     }
 }
