b71d965ed4ded126d9ebabb4c8015b518231f783
Jack <Gommeren@msn.com>
2022-01-07 15:02:53 +0100

    Updated requestInterceptor (background) to store intercepted values

--- src/background/requestInterceptor.ts
+++ src/background/requestInterceptor.ts
@@ -1,28 +1,58 @@
+import { DataFieldTypes } from "src/content/interfaces/data/dataFieldTypes.interface";
+import { DataRecordValues } from "src/content/interfaces/data/dataRecordValues.interface";
+import { portMessage } from "src/content/interfaces/portMessage.interface";
+
 export class requestInterceptor {
-  private activeTabId: number | undefined;
+  public activeTabId: number | undefined;
   public xAuthToken: string | undefined;
 
   //TODO: Refactor this so contentscript asks bg script for this info instead of other way around.
   private isMessageSent = false;
 
+  private lastAction: RequestStoredValues = {
+    likedPerson: null,
+    superLikedPerson: null,
+    passedPerson: null
+  };
+
   constructor() {
-    chrome.webRequest.onBeforeSendHeaders.addListener(
-      (details: chrome.webRequest.WebRequestHeadersDetails) => {
-        // (details:any) => {
-        if (!this.isMainRequest(details)) {
+    chrome.webRequest.onBeforeRequest.addListener(
+      (details: chrome.webRequest.WebRequestFullDetails) => {
+
+        if (!this.isTinderRequest(details)) {
           return;
         }
 
-        if (this.isMessageSent) {
-          return;
+        let action: 'likedPersonId' | 'superLikedPerson' | 'passedPersonId' | undefined;
+        let dataField:DataRecordValues | undefined;
+
+        switch (true) {
+          case details.url.startsWith('https://api.gotinder.com/like/'):
+            action = 'likedPersonId';
+            dataField = this._getDataRecordValueNewPerson(details.url);
+            this.lastAction.likedPerson = dataField;
+            break;
+          case details.url.startsWith('https://api.gotinder.com/superlike/'): //todo: check if this is correct
+            action = 'superLikedPerson';
+            dataField = this._getDataRecordValueNewPerson(details.url);
+            this.lastAction.superLikedPerson = dataField;
+            break;
+          case details.url.startsWith('https://api.gotinder.com/pass/'):
+            action = 'passedPersonId';
+            dataField = this._getDataRecordValueNewPerson(details.url);
+            this.lastAction.passedPerson = dataField;
+            break;
+          default:
+            break;
         }
 
-        this.xAuthToken = details.requestHeaders ? this.getXAUthTokenFromRequestHeadersList(details.requestHeaders) : undefined;
+        // if(action && dataField){
+        //   this.sendMessageToContent(action, dataField);
+        // }
 
-        this.sendMessageToContent();
       },
       { urls: ["https://api.gotinder.com/*"] },
-      ["requestHeaders"]
+      ['requestBody']
     );
 
     // chrome.webRequest.onAuthRequired.addListener((watisthis)=>{
@@ -32,53 +62,68 @@ export class requestInterceptor {
     //   debugger;
     // });
 
-    chrome.tabs.onActivated.addListener((activeInfo) => {
-      console.dir(`Got the activeTabId! It is: ${activeInfo.tabId}`);
-      this.activeTabId = this.getTinderTabId(activeInfo.tabId) ? activeInfo.tabId : undefined;
+    chrome.tabs.onActivated.addListener((activeTabInfo:chrome.tabs.TabActiveInfo) => {
+      console.dir(`Activetab changed! The id is now: ${activeTabInfo.tabId}`);
+      this.setTinderTabId();
     });
 
   }
-
-  //TODO: Should be refactored to use interface RequestHeader, but error; 'should be HTTPHeader' prevents this
-  private getXAUthTokenFromRequestHeadersList(requestHeaders: any[]): string {
-    const xAuthHeader: RequestHeader = requestHeaders.find((requestHeader: RequestHeader) => {
-      if (requestHeader.name === "X-Auth-Token" && requestHeader.value) {
-        return requestHeader.value;
+  private _getDataRecordValueNewPerson(url: string): DataRecordValues {
+    return {
+      'label': 'System-no', 
+      'value': {
+        'appType': 'tinder', 
+        'id': this._getPersonIdFromUrl(url)
       }
-    });
-    console.dir(`Got the xAuthToken! It is: ${xAuthHeader.value}`);
-    return xAuthHeader.value;
+    };
+  }
+  private _getPersonIdFromUrl(url: string): string {
+    const urlStrippedStart: string = url.replace('https://api.gotinder.com/like/', '');
+    return urlStrippedStart.slice(0, urlStrippedStart.indexOf('?'));
   }
 
-  private sendMessageToContent(): void {
-    if (this.xAuthToken && this.activeTabId) {
-      chrome.tabs.sendMessage(this.activeTabId, this.xAuthToken, {}, () => {
-        console.log(`XAuthToken retrieved & sent! xAuthToken: ${this.xAuthToken} & activeTabId: ${this.activeTabId}`);
-        this.isMessageSent = true;
+  private sendMessageToContent(action: string, dataField: DataRecordValues): void {
+    console.log('going to send message before if');
+    console.dir(`active tab is: ${this.activeTabId}`);
+    if (this.activeTabId) {
+      console.log('going to send message after if');
+      const port = chrome.tabs.connect(this.activeTabId, <chrome.runtime.Port>{name: "knockknock"});
+      
+      port.postMessage(<portMessage>{
+        'message': 'background', 
+        'action': action,
+        'payload': [
+          dataField
+        ]
       });
+      console.log(`message sent from background!`);
     }
   }
 
-  private isMainRequest(details: chrome.webRequest.WebRequestHeadersDetails): boolean {
-    return details.method === "POST" && details.initiator && details.initiator === "https://tinder.com" ? true : false
+  private isTinderRequest(details: chrome.webRequest.WebRequestHeadersDetails): boolean {
+    return details.initiator && details.initiator === "https://tinder.com" ? true : false;
   }
-  private getTinderTabId(tabId:number):number | undefined{
+
+  private setTinderTabId():void {
     let tinderTab:chrome.tabs.Tab | undefined;
-    chrome.tabs.query({ active: true, currentWindow: true }, function (arrayOfTabs: chrome.tabs.Tab[]) {
+    chrome.tabs.query({ active: true, currentWindow: true }, (arrayOfTabs: chrome.tabs.Tab[]) => {
       tinderTab = arrayOfTabs.find((tab: chrome.tabs.Tab) => {
         return tab.title && tab.title.includes('Tinder');
       });
+      if(tinderTab && tinderTab.id){
+        console.dir(`Found the tinder tab id! It is: ${tinderTab.id}`);
+        this.activeTabId = tinderTab.id;
+      }
     });
-    if(tinderTab && tinderTab.id){
-      console.dir(`Found the tinder tab id! It is: ${tinderTab.id}`);
-      return tinderTab.id
-    }
-    return undefined;
   }
-
-};
+}
 
 interface RequestHeader {
   name: string;
   value: string;
+}
+interface RequestStoredValues {
+  likedPerson: DataRecordValues | null,
+  superLikedPerson: DataRecordValues | null,
+  passedPerson: DataRecordValues | null
 }
