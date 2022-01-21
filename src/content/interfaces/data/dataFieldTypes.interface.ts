f53729347bc0ab4dd300d5fea56930e0515e3659
Jack <Gommeren@msn.com>
2022-01-21 23:05:26 +0100

    Added UIRequiredType as a supported type of requiredFieldType to DataFieldTypes

--- src/content/interfaces/data/dataFieldTypes.interface.ts
+++ src/content/interfaces/data/dataFieldTypes.interface.ts
@@ -1,4 +1,8 @@
+import { UIRequiredType } from "src/content/classes/data/dataField";
+
 export interface DataFieldTypes {
     label: string,
+    dataType: string,
+    requiredFieldType: UIRequiredType | null,
     checkDataMethod: (dataEntry: unknown) => boolean
 }
