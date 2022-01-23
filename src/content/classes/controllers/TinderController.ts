e40aba43f3ce86e21a9b062249109786497f9f19
Jack <Gommeren@msn.com>
2022-01-23 18:49:06 +0100

    Added new data record to table after submitEvent (e.g. like or pass) in TinderController

--- src/content/classes/controllers/TinderController.ts
+++ src/content/classes/controllers/TinderController.ts
@@ -611,6 +611,7 @@ export class TinderController implements datingAppController {
                                     value: personActionStatus
                                 }]);
 
+                                this.dataTable.addNewDataRecord(newDataRecord);
                             }
     
                             this.uiRenderer.setLoadingOverlay('loadingSwipeAction', false);
