23087f5663aa66263dbd9332f3076a18c056f398
Jack <Gommeren@msn.com>
2022-02-07 10:38:35 +0100

    Updated dataTable addNewDataRecord; refactored logic to getNo if exists, set No if it doesn't and add record. Log error when found No does not match the incremented No currently present in dataTable

--- src/content/classes/data/dataTable.ts
+++ src/content/classes/data/dataTable.ts
@@ -3,7 +3,7 @@ import { DataField } from "./dataField";
 import { DataRecord } from "./dataRecord";
 
 export class dataTable {
-    
+
     private dataRecords: DataRecord[] = [];
     private dataRecordAmount = 0;
 
@@ -12,14 +12,14 @@ export class dataTable {
     }
 
     //todo: refactor to get record directly? 
-    public getRecordIndexBySystemId(systemId: string, appType: string):number {
-        return this.dataRecords.findIndex((dataRecord: DataRecord)=>{
+    public getRecordIndexBySystemId(systemId: string, appType: string): number {
+        return this.dataRecords.findIndex((dataRecord: DataRecord) => {
             return dataRecord.getRecordPersonSystemId(appType) === systemId;
         });
     }
 
     public getRecordByRecordIndex(index: number): DataRecord | null {
-        if(index >= 0 && index <= this.dataRecords.length){
+        if (index >= 0 && index <= this.dataRecords.length) {
             return this.dataRecords[index];
         }
         console.error('Data record not found. Please use a different number');
@@ -28,19 +28,24 @@ export class dataTable {
 
     public addNewDataRecord(dataRecord: DataRecord): boolean {
 
-        // if dataRecord has no (valid) number, assign a number by the current count for this table
-        if(!dataRecord.getNoDataRecord()){
-            dataRecord.setNoDataRecord(this.dataRecordAmount+1);
+        // if dataRecord has no number, assign a number by the current (incremented) count for this table & add record
+        const currentRecordNo: number | null = dataRecord.getNoDataRecord();
+        if (currentRecordNo === null) {
+            dataRecord.setNoDataRecord(this.dataRecordAmount + 1);
+            this.dataRecords.push(dataRecord);
             this.incrementRecordAmount();
-        }else{
-            if(<number>dataRecord.getNoDataRecord() !== (this.dataRecordAmount+1)){
-                console.error(`Invalid data record number: ${dataRecord.getNoDataRecord()} for record: ${dataRecord}. Data record number must be incremented from the current data record amount: ${this.dataRecordAmount}`);
-                return false;
-            }
-            // if dataRecord has a number, number must be incrementable from start of app
+            return true;
         }
-        
+
+        // if dataRecord has an invalid number, log error
+        if (currentRecordNo !== (this.dataRecordAmount + 1)) {
+            console.error(`Invalid data record number: ${dataRecord.getNoDataRecord()} for record: ${dataRecord}. Data record number must be incremented from the current data record amount: ${this.dataRecordAmount}`);
+            return false;
+        }
+
+        // if dataRecord has a number, number must be incrementable from start of app
         this.dataRecords.push(dataRecord);
+        this.incrementRecordAmount();
         return true;
     }
 
@@ -48,7 +53,7 @@ export class dataTable {
         this.dataRecordAmount = this.dataRecordAmount + 1;
     }
 
-    public updateDataRecordByIndex(index:number, dataRecord: DataRecordValues[]):void {
+    public updateDataRecordByIndex(index: number, dataRecord: DataRecordValues[]): void {
         this.dataRecords[index].addDataToDataFields(dataRecord);
     }
 }
