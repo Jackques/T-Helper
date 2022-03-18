d56648cda23ca080c0daf21aacdc9fd53517e837
Jack <Gommeren@msn.com>
2022-03-18 16:35:01 +0100

    Added feature to save imported filename, update it and use it for export

--- src/content/interfaces/portMessage.interface.ts
+++ src/content/interfaces/portMessage.interface.ts
@@ -1,5 +1,5 @@
 export interface PortMessage {
     messageSender: string, // BACKGROUND CONTENT POPUP
     action: string, // SUBMIT_ACTION OR INIT
-    payload: any[] //todo: Add type my future records array
+    payload: string | any[] //todo: Add type my future records array
 }
