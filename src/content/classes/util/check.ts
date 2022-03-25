915e889f91655e0507b5328be8c91ac64156d238
Jack <Gommeren@msn.com>
2022-03-25 20:53:08 +0100

    Updated Check.isValidDate() to use momentJS for checking date instead of default Date()

--- src/content/classes/util/check.ts
+++ src/content/classes/util/check.ts
@@ -1,6 +1,11 @@
+import * as moment from 'moment';
+import 'moment/locale/nl';
+
+moment.locale('nl');
+
 export class Check {
     static isValidDate(dateString: string): boolean {
-        return new Date(dateString) ? true : false;
+        return moment.default(dateString, true).isValid() ? true : false;
     }
     static isPositiveNumberEntry(numberEntry: number): boolean {
         return numberEntry >= 0 ? true : false;
